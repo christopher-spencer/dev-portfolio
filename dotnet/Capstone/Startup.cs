660306b7fe630fffbc1412adc84d26a249e1b859
thebellywolf <cdspenzer@gmail.com>
2024-02-15 18:48:49 -0500

    trying to switch to postgresql database

--- dotnet/Capstone/Startup.cs
+++ dotnet/Capstone/Startup.cs
@@ -63,9 +63,6 @@ namespace Capstone
             services.AddSingleton<ITokenGenerator>(tk => new JwtGenerator(Configuration["JwtSecret"]));
             services.AddSingleton<IPasswordHasher>(ph => new PasswordHasher());
             services.AddTransient<IUserDao>(m => new UserPostgresDao(connectionString));
-
-            // services.AddTransient<NpgsqlConnection>(_ => new NpgsqlConnection(connectionString));
-            // services.AddTransient<IUserDao>(m => new UserPostgresDao(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -1,31 +1,16 @@
-USE master
-GO
+BEGIN TRANSACTION;
 
---drop database if it exists
-IF DB_ID('final_capstone') IS NOT NULL
-BEGIN
-	ALTER DATABASE final_capstone SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE final_capstone;
-END
+DROP TABLE IF EXISTS users;
 
-CREATE DATABASE final_capstone
-GO
-
-USE final_capstone
-GO
-
---create tables
 CREATE TABLE users (
-	user_id int IDENTITY(1,1) NOT NULL,
-	username varchar(50) NOT NULL,
-	password_hash varchar(200) NOT NULL,
-	salt varchar(200) NOT NULL,
-	user_role varchar(50) NOT NULL
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
-)
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(200) NOT NULL,
+    salt VARCHAR(200) NOT NULL,
+    user_role VARCHAR(50) NOT NULL
+);
 
---populate default data
-INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
-INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user', 'Jg45HuwT7PZkfuKTz6IB90CtWY4=', 'LHxP4Xh7bN0=', 'user');
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin', 'YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=', 'admin');
 
-GO
+COMMIT TRANSACTION;
