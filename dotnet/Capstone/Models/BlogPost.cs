de7d8a6abf95c645516e907398c1918796c28949
thebellywolf <cdspenzer@gmail.com>
2024-02-17 16:13:41 -0500

    updated BP model properties

---
+++ dotnet/Capstone/Models/BlogPost.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Capstone.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public string ImageName { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
