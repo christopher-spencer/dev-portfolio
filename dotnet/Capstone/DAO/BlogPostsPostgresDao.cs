d2d3772d9d6cd03fd7b699bfe35539b1807fec6e
thebellywolf <cdspenzer@gmail.com>
2024-02-17 16:48:22 -0500

    created MapRow to Blog Post

--- dotnet/Capstone/DAO/BlogPostsPostgresDao.cs
+++ dotnet/Capstone/DAO/BlogPostsPostgresDao.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using Capstone.DAO.Interfaces;
 using Capstone.Models;
+using Npgsql;
 
 namespace Capstone.DAO
 {
@@ -16,9 +19,40 @@ namespace Capstone.DAO
         {
             List<BlogPost> blogPosts = new List<BlogPost>();
 
-            
+            string sql = "SELECT blogpost_id, blogpost_name, blogpost_author, blogpost_description, " +
+                "blogpost_content, image_name, image_url, created_at, updated_at " +
+                "FROM blogposts;";
+
+            try 
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    NpgsqlCommand cmd = new NpgsqlCommand(sql, connection);
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+                }
+            }
 
             return blogPosts;
+        }
+
+        private BlogPost MapRowToBlogPost(NpgsqlDataReader reader)
+        {
+            BlogPost blogPost = new BlogPost
+            {
+                Id = Convert.ToInt32(reader["blogpost_id"]),
+                Name = Convert.ToString(reader["blogpost_name"]),
+                Author = Convert.ToString(reader["blogpost_author"]),
+                Description = Convert.ToString(reader["blogpost_description"]),
+                Content = Convert.ToString(reader["blogpost_content"]),
+                ImageName = Convert.ToString(reader["image_name"]),
+                ImageUrl = Convert.ToString(reader["image_url"]),
+                CreatedAt = Convert.ToDateTime(reader["created_at"]),
+                UpdatedAt = Convert.ToDateTime(reader["updated_at"])
+            };
+
+            return blogPost;
         }
 
     }
--- dotnet/Capstone/Models/BlogPost.cs
+++ dotnet/Capstone/Models/BlogPost.cs
@@ -7,6 +7,7 @@ namespace Capstone.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
+        public string Description { get; set; }
         public string Content { get; set; }
         public string ImageName { get; set; }
         public string ImageUrl { get; set; }
