93ba428bf9e8fcebf17d0b50e61a0e5b204ce2f9
thebellywolf <cdspenzer@gmail.com>
2024-02-19 12:51:36 -0500

    additional project model updates

--- dotnet/Capstone/Models/SideProject.cs
+++ dotnet/Capstone/Models/SideProject.cs
@@ -9,10 +9,18 @@ namespace Capstone.Models
         public string Name { get; set; }
         public string MainImageUrl { get; set; }
         public string Description { get; set; }
+        public List<string> GoalsAndObjectives { get; set; }
         public List<string> AdditionalImagesUrl { get; set; }
         public string VideoWalkthroughUrl { get; set; }
         public List<TechSkill> ToolsUsed { get; set; }
         public string WebsiteLink { get; set; }
         public string GitHubRepoLink { get; set; }
+        public List<string> Contributors { get; set; }
+        public List<string> ExternalAPIsAndServicesUsed { get; set; }
+        public List<string> DependenciesOrLibrariesUsed { get; set; }
+        public string ProjectStatus { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+
     }
 }
