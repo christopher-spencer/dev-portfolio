43cf501ae78ea59bf362675d1278e34e54f3b577
thebellywolf <cdspenzer@gmail.com>
2024-02-19 18:14:22 -0500

    successful user unit tests :)

--- dotnet/DevPortfolioTests/PostgresDaoTestBase.cs
+++ dotnet/DevPortfolioTests/PostgresDaoTestBase.cs
@@ -1,4 +1,5 @@
 using System.Transactions;
+using Npgsql;
 
 namespace Capstone.UnitTests.DAO
 {
@@ -12,6 +13,18 @@ namespace Capstone.UnitTests.DAO
         public void Initialize()
         {
             transaction = new TransactionScope();
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                string sql_insert = "INSERT INTO users (username, password_hash, salt, user_role) " +
+                    "VALUES ('notauser', 'jjjjjjjjj', 'kkkkkkkkkk', 'user');";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql_insert, conn);
+                int count = cmd.ExecuteNonQuery();
+
+                Assert.AreEqual(1, count, "Insert into user failed");
+            }
         }
 
         [TestCleanup]
