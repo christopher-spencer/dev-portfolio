e27fe619470bce4920ed45100417a0618d732db4
thebellywolf <cdspenzer@gmail.com>
2024-02-20 12:08:39 -0500

    added Image model

---
+++ dotnet/Capstone/Models/Image.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Capstone.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
