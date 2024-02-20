173e5a1e79aa3c57385991692dd8e66679bb97b8
thebellywolf <cdspenzer@gmail.com>
2024-02-20 11:54:59 -0500

    created Hobby model and updated maprow in PortfolioPGDAO

--- dotnet/Capstone/Models/Portfolio/Portfolio.cs
+++ dotnet/Capstone/Models/Portfolio/Portfolio.cs
@@ -7,9 +7,10 @@ namespace Capstone.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string PortfolioImage { get; set; }
         public string Location { get; set; }
         public string ProfessionalSummary { get; set; }
-        public List<string> InterestsAndHobbies { get; set; }
+        public List<string> Hobbies { get; set; }
         public string Email { get; set; }
         public string GitHubURL { get; set; }
         public string LinkedInURL { get; set; }
