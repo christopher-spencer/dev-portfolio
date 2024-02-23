07624fc4eda592e7b4e2f6fa70bbfc7dace8d282
thebellywolf <cdspenzer@gmail.com>
2024-02-23 16:51:56 -0500

    added WebsiteId and LogoId to DependencyLibrary model

--- dotnet/Capstone/Models/SideProject/SideProjectModels/DependencyLibrary.cs
+++ dotnet/Capstone/Models/SideProject/SideProjectModels/DependencyLibrary.cs
@@ -8,7 +8,9 @@ namespace Capstone.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int WebsiteId { get; set; }
         public Website Website { get; set; }
+        public int LogoId { get; set; }
         public Image Logo { get; set; }
     }
 }
