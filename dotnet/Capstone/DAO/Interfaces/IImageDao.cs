8657f0a5a0afc20c1883ff40a6b974ca7a8e680c
thebellywolf <cdspenzer@gmail.com>
2024-02-23 14:09:32 -0500

    update ImagePostgresDao

--- dotnet/Capstone/DAO/Interfaces/IImageDao.cs
+++ dotnet/Capstone/DAO/Interfaces/IImageDao.cs
@@ -6,6 +6,7 @@ namespace Capstone.DAO.Interfaces
     public interface IImageDao
     {
         Image CreateImage(Image image);
+        Image CreateImageByProjectId(int projectId, Image image);
         List<Image> GetImagesByProjectId(int projectId);
         Image GetImageByProjectId(int projectId);
         Image GetImageByProjectIdAndImageId(int projectId, int imageId);
@@ -13,5 +14,6 @@ namespace Capstone.DAO.Interfaces
         List<Image> GetAllImages();
         Image UpdateImage(Image image);
         int DeleteImageById(int imageId);
+        int DeleteImageByProjectId(int projectId, int imageId);
     }
 }
--- dotnet/Capstone/DAO/PostgresDao/ImagePostgresDao.cs
+++ dotnet/Capstone/DAO/PostgresDao/ImagePostgresDao.cs
@@ -43,14 +43,85 @@ namespace Capstone.DAO
             return image;
         }
 
+        public Image CreateImageByProjectId(int projectId, Image image)
+        {
+            string insertImageSql = "INSERT INTO images (name, url) VALUES (@name, @url) RETURNING id;";
+            string insertSideProjectImageSql = "INSERT INTO sideproject_images (sideproject_id, image_id) VALUES (@projectId, @imageId);";
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(insertImageSql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@name", image.Name);
+                        cmd.Parameters.AddWithValue("@url", image.Url);
+
+                        int id = Convert.ToInt32(cmd.ExecuteScalar());
+                        image.Id = id;
+                    }
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(insertSideProjectImageSql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@projectId", projectId);
+                        cmd.Parameters.AddWithValue("@imageId", image.Id);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                throw new DaoException("An error occurred while creating the image.", ex);
+            }
+
+            return image;
+        }
+
+        public Image GetImageByProjectId(int projectId)
+        {
+            Image image = null;
+
+            string sql = "SELECT i.id, i.name, i.url " +
+                         "FROM images i " +
+                         "JOIN sideproject_images spi ON i.id = spi.image_id " +
+                         "WHERE spi.sideproject_id = @projectId";
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    NpgsqlCommand cmd = new NpgsqlCommand(sql, connection);
+                    cmd.Parameters.AddWithValue("@projectId", projectId);
+
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        image = MapRowToImage(reader);
+                    }
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                throw new DaoException("An error occurred while retrieving the image by project ID.", ex);
+            }
+
+            return image;
+        }
+
         public List<Image> GetImagesByProjectId(int projectId)
         {
             List<Image> images = new List<Image>();
 
             string sql = "SELECT i.id, i.name, i.url " +
                          "FROM images i " +
-                         "JOIN side_project_images spi ON i.id = spi.image_id " +
-                         "WHERE spi.project_id = @projectId;";
+                         "JOIN sideproject_images spi ON i.id = spi.image_id " +
+                         "WHERE spi.sideproject_id = @projectId;";
 
             try
             {
@@ -78,50 +149,14 @@ namespace Capstone.DAO
             return images;
         }
 
-        public Image GetImageByProjectId(int projectId)
-        {
-            Image image = null;
-
-            string sql = "SELECT i.id, i.name, i.url " +
-                         "FROM images i " +
-                         "JOIN side_project_images spi ON i.id = spi.image_id " +
-                         "WHERE spi.project_id = @projectId";
-
-            try
-            {
-                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
-                {
-                    connection.Open();
-
-                    NpgsqlCommand cmd = new NpgsqlCommand(sql, connection);
-                    cmd.Parameters.AddWithValue("@projectId", projectId);
-
-                    NpgsqlDataReader reader = cmd.ExecuteReader();
-
-                    if (reader.Read())
-                    {
-                        image = MapRowToImage(reader);
-                    }
-                }
-            }
-            catch (NpgsqlException ex)
-            {
-                throw new DaoException("An error occurred while retrieving the image by project ID.", ex);
-            }
-
-            return image;
-        }
-
-        // TODO GetImageByProjectNameAndImageName (?)
         public Image GetImageByProjectIdAndImageId(int projectId, int imageId)
         {
             Image image = null;
 
             string sql = "SELECT i.id, i.name, i.url " +
                          "FROM images i " +
-                         "JOIN side_project_images spi ON i.id = spi.image_id " +
-                         "WHERE spi.project_id = @projectId " +
-                         "AND i.id = @imageId;";
+                         "JOIN sideproject_images spi ON i.id = spi.image_id " +
+                         "WHERE spi.sideproject_id = @projectId AND i.id = @imageId;";
 
             try
             {
@@ -249,6 +284,39 @@ namespace Capstone.DAO
                     cmd.Parameters.AddWithValue("@id", imageId);
 
                     return cmd.ExecuteNonQuery();
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                throw new DaoException("An error occurred while deleting the image.", ex);
+            }
+        }
+
+        public int DeleteImageByProjectId(int projectId, int imageId)
+        {
+            string deleteSideProjectImageSql = "DELETE FROM sideproject_images WHERE sideproject_id = @projectId AND image_id = @imageId;";
+            string deleteImageSql = "DELETE FROM images WHERE id = @imageId;";
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(deleteSideProjectImageSql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@projectId", projectId);
+                        cmd.Parameters.AddWithValue("@imageId", imageId);
+
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(deleteImageSql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@imageId", imageId);
+
+                        return cmd.ExecuteNonQuery();
+                    }
                 }
             }
             catch (NpgsqlException ex)
