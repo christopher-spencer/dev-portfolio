f0088c015859f396b05ae0b75098e55ca23d6ff6
thebellywolf <cdspenzer@gmail.com>
2024-03-01 15:19:42 -0500

    added Comment about additional fields to think about in Blog Post model

--- dotnet/Capstone/Models/BlogPost/BlogPost.cs
+++ dotnet/Capstone/Models/BlogPost/BlogPost.cs
@@ -15,5 +15,17 @@ namespace Capstone.Models
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
-
+    /*
+        Categories or Tags - Allow categorization or tagging of blog posts to make them easier to organize and search.
+        Comments - Include a collection of comments associated with each blog post.
+        Likes or Up-votes - Could include a counter for the number of likes or up-votes a blog post receives.
+        Views or Page Visits - Keep track of how many times a blog post has been viewed to gauge its popularity.
+        Status - Include a field to indicate the status of the blog post (e.g., draft, published, archived).
+        SEO Metadata - Add fields for SEO metadata such as meta title, meta description, and keywords to optimize search engine visibility.
+        Related Posts - Include a list of related posts to encourage further reading and engagement.
+        Author Information - If eventually multiple authors/contributors, could associate posts with additional author info or links 
+            like bio, profile picture, or contact details.
+        Featured Post Flag - Mark a post as featured, which could be used for highlighting important or noteworthy content.
+        Attachments - Allow for the attachment of files or documents related to posts, such as PDFs, images, or other media.
+    */
 }
