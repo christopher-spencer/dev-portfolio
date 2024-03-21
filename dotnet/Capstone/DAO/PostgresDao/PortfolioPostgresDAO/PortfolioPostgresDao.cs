0a97d7d6cda5c47c9e32cf003bc8872e98293b50
thebellywolf <cdspenzer@gmail.com>
2024-03-21 17:13:17 -0400

    added GetPortfolio and GetPortfolios

--- dotnet/Capstone/DAO/PostgresDao/PortfolioPostgresDAO/PortfolioPostgresDao.cs
+++ dotnet/Capstone/DAO/PostgresDao/PortfolioPostgresDAO/PortfolioPostgresDao.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using Capstone.DAO.Interfaces;
+using Capstone.Exceptions;
 using Capstone.Models;
 using Npgsql;
 
@@ -14,20 +16,92 @@ namespace Capstone.DAO
         private readonly ISkillDao _skillDao;
 
         public PortfolioPostgresDao(string dbConnectionString, ISideProjectDao sideProjectDao,
-            IWebsiteDao websiteDao, IImageDao imageDao, ISkillDao skillDao) {
+            IWebsiteDao websiteDao, IImageDao imageDao, ISkillDao skillDao)
+        {
             connectionString = dbConnectionString;
             this._sideProjectDao = sideProjectDao;
             this._websiteDao = websiteDao;
             this._imageDao = imageDao;
             this._skillDao = skillDao;
         }
-// TODO will need to model after SP and BP Daos
-// TODO use SIDEPROJECT POSTGRES DAO CRUD AS TEMPLATE FOR IMPROVING CRUD METHODS
+
         /*  
             **********************************************************************************************
                                             PORTFOLIO CRUD
             **********************************************************************************************
         */
+
+        public List<Portfolio> GetPortfolios()
+        {
+            List<Portfolio> portfolios = new List<Portfolio>();
+
+            string sql = "SELECT id, name, location, professional_summary, email FROM portfolios";
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Portfolio portfolio = MapRowToPortfolio(reader);
+                                portfolios.Add(portfolio);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                throw new DaoException("An error occurred while retrieving the portfolios.", ex);
+            }
+
+            return portfolios;
+        }
+
+        public Portfolio GetPortfolio(int portfolioId)
+        {
+            if (portfolioId <= 0)
+            {
+                throw new ArgumentException("PortfolioId must be greater than zero.");
+            }
+
+            Portfolio portfolio = null;
+
+            string sql = "SELECT id, name, location, professional_summary, email FROM portfolios WHERE id = @portfolioId";
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@portfolioId", portfolioId);
+
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                portfolio = MapRowToPortfolio(reader);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                throw new DaoException("An error occurred while retrieving the portfolio.", ex);
+            }
+
+            return portfolio;
+        }
 
         /*  
             **********************************************************************************************
