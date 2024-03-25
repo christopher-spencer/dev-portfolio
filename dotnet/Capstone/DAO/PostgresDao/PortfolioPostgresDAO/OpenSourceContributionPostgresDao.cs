5991f5b2182a2db3c5b620aa825926f3d19b44a3
thebellywolf <cdspenzer@gmail.com>
2024-03-25 11:02:38 -0400

    added HelperMethods to OpenSourceContributionPGDAO

--- dotnet/Capstone/DAO/PostgresDao/PortfolioPostgresDAO/OpenSourceContributionPostgresDao.cs
+++ dotnet/Capstone/DAO/PostgresDao/PortfolioPostgresDAO/OpenSourceContributionPostgresDao.cs
@@ -48,6 +48,238 @@ namespace Capstone.DAO
             **********************************************************************************************
         */
 
+        private int? GetOrganizationLogoIdByOpenSourceContributionId(int contributionId)
+        {
+            string sql = "SELECT organization_logo_id FROM open_source_contributions WHERE id = @contributionId;";
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("contributionId", contributionId);
+
+                        object result = cmd.ExecuteScalar();
+
+                        if (result != null && result != DBNull.Value)
+                        {
+                            return Convert.ToInt32(result);
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error retrieving Organization Logo ID by Open Source Contribution ID: " + ex.Message);
+                return null;
+            }
+        }
+
+        private int? GetOrganizationWebsiteIdByOpenSourceContributionId(int contributionId)
+        {
+            string sql = "SELECT organization_website_id FROM open_source_contributions WHERE id = @contributionId;";
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("contributionId", contributionId);
+
+                        object result = cmd.ExecuteScalar();
+
+                        if (result != null && result != DBNull.Value)
+                        {
+                            return Convert.ToInt32(result);
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error retrieving Organization Website ID by Open Source Contribution ID: " + ex.Message);
+                return null;
+            }
+        }
+
+        private int? GetOrganizationGitHubIdByOpenSourceContributionId(int contributionId)
+        {
+            string sql = "SELECT organization_github_id FROM open_source_contributions WHERE id = @contributionId;";
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("contributionId", contributionId);
+
+                        object result = cmd.ExecuteScalar();
+
+                        if (result != null && result != DBNull.Value)
+                        {
+                            return Convert.ToInt32(result);
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error retrieving Organization GitHub ID by Open Source Contribution ID: " + ex.Message);
+                return null;
+            }
+        }
+
+        private int? GetMainImageIdByOpenSourceContributionId(int contributionId)
+        {
+            string sql = "SELECT main_image_id FROM open_source_contributions WHERE id = @contributionId;";
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("contributionId", contributionId);
+
+                        object result = cmd.ExecuteScalar();
+
+                        if (result != null && result != DBNull.Value)
+                        {
+                            return Convert.ToInt32(result);
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error retrieving Main Image ID by Open Source Contribution ID: " + ex.Message);
+                return null;
+            }
+        }
+
+        private int DeleteTechSkillsUtilizedByOpenSourceContributionId(int contributionId)
+        {
+            List<Skill> skills = _skillDao.GetSkillsByOpenSourceContributionId(contributionId);
+
+            int skillsDeletedCount = 0;
+
+            foreach (Skill skill in skills)
+            {
+                int skillId = skill.Id;
+
+                try
+                {
+                    _skillDao.DeleteSkillByOpenSourceContributionId(contributionId, skillId);
+                    skillsDeletedCount++;
+                }
+                catch
+                {
+                    Console.WriteLine("Error deleting Skill by Open Source Contribution ID: " + skillId);
+                }
+            }
+
+            return skillsDeletedCount;
+        }
+
+        private int DeletePullRequestsLinksByOpenSourceContributionId(int contributionId)
+        {
+            List<Website> websites = _websiteDao.GetWebsitesByOpenSourceContributionId(contributionId);
+
+            int websitesDeletedCount = 0;
+
+            foreach (Website website in websites)
+            {
+                int websiteId = website.Id;
+
+                try
+                {
+                    _websiteDao.DeleteWebsiteByOpenSourceContributionId(contributionId, websiteId);
+                    websitesDeletedCount++;
+                }
+                catch
+                {
+                    Console.WriteLine("Error deleting Website by Open Source Contribution ID: " + websiteId);
+                }
+            }
+
+            return websitesDeletedCount;
+        }
+
+        private int DeleteReviewCommentsAndFeedbackReceivedByOpenSourceContributionId(int contributionId)
+        {
+            List<Achievement> achievements = _achievementDao.GetAchievementsByOpenSourceContributionId(contributionId);
+
+            int achievementsDeletedCount = 0;
+
+            foreach (Achievement achievement in achievements)
+            {
+                int achievementId = achievement.Id;
+
+                try
+                {
+                    _achievementDao.DeleteAchievementByOpenSourceContributionId(contributionId, achievementId);
+                    achievementsDeletedCount++;
+                }
+                catch
+                {
+                    Console.WriteLine("Error deleting Achievement by Open Source Contribution ID: " + achievementId);
+                }
+            }
+
+            return achievementsDeletedCount;
+        }
+
+        private int DeleteAdditionalImagesByOpenSourceContributionId(int contributionId)
+        {
+            List<Image> additionalImages = _imageDao.GetAdditionalImagesByOpenSourceContributionId(contributionId);
+
+            int imagesDeletedCount = 0;
+
+            foreach (Image image in additionalImages)
+            {
+                int imageId = image.Id;
+
+                try
+                {
+                    _imageDao.DeleteImageByOpenSourceContributionId(contributionId, imageId);
+                    imagesDeletedCount++;
+                }
+                catch
+                {
+                    Console.WriteLine("Error deleting Additional Image by Open Source Contribution ID: " + imageId);
+                }
+            }
+
+            return imagesDeletedCount;
+        }
+
         /*  
             **********************************************************************************************
                                             OPEN SOURCE CONTRIBUTION MAP ROW
