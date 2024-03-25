e2cb52b17fd58a2d8602fdd5d943051471bf0c2c
thebellywolf <cdspenzer@gmail.com>
2024-03-25 13:11:31 -0400

    added VolunteerWork CRUD to IVWDao

--- dotnet/Capstone/DAO/Interfaces/PortfolioInterfaces/IVolunteerWorkDao.cs
+++ dotnet/Capstone/DAO/Interfaces/PortfolioInterfaces/IVolunteerWorkDao.cs
@@ -10,11 +10,22 @@ namespace Capstone.DAO.Interfaces
                                             VOLUNTEER WORK CRUD
             **********************************************************************************************
         */
-
+// TODO VOLUNTEER WORK Controllers****
+        VolunteerWork CreateVolunteerWork(VolunteerWork volunteerWork);
+        List<VolunteerWork> GetVolunteerWorks();
+        VolunteerWork GetVolunteerWork(int volunteerWorkId);
+        VolunteerWork UpdateVolunteerWork(int volunteerWorkId, VolunteerWork volunteerWork);
+        int DeleteVolunteerWork(int volunteerWorkId);
         /*  
             **********************************************************************************************
                                             PORTFOLIO VOLUNTEER WORK CRUD
             **********************************************************************************************
         */
+// TODO create PORTFOLIO Volunteer Work Controllers
+        VolunteerWork CreateVolunteerWorkByPortfolioId(int portfolioId, VolunteerWork volunteerWork);
+        List<VolunteerWork> GetVolunteerWorksByPortfolioId(int portfolioId);
+        VolunteerWork GetVolunteerWorkByPortfolioId(int portfolioId, int volunteerWorkId);
+        VolunteerWork UpdateVolunteerWorkByPortfolioId(int portfolioId, int volunteerWorkId, VolunteerWork volunteerWork);
+        int DeleteVolunteerWorkByPortfolioId(int portfolioId, int volunteerWorkId);
     }
 }
