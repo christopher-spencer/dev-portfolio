b7a29d3ff06b17a4759be049810d84e530bcadb0
thebellywolf <cdspenzer@gmail.com>
2024-03-28 16:38:11 -0400

    savepoint

--- dotnet/Capstone/DAO/PostgresDao/PortfolioPostgresDAO/EducationPostgresDao.cs
+++ dotnet/Capstone/DAO/PostgresDao/PortfolioPostgresDAO/EducationPostgresDao.cs
@@ -360,7 +360,7 @@ namespace Capstone.DAO
                                 cmd.Parameters.AddWithValue("@fieldOfStudy", education.FieldOfStudy);
                                 cmd.Parameters.AddWithValue("@major", education.Major);
     // FIXME example line below to check for NULL or Empty????
-                                // cmd.Parameters.AddWithValue("@major", (object)education.Major ?? DBNull.Value);
+                                //cmd.Parameters.AddWithValue("@major", (object)education.Major ?? DBNull.Value);
                                 cmd.Parameters.AddWithValue("@minor", education.Minor);
                                 cmd.Parameters.AddWithValue("@degreeObtained", education.DegreeObtained);
                                 cmd.Parameters.AddWithValue("@gpaOverall", education.GPAOverall);
