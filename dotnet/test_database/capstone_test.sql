5e9b1dd2fbd208078b5a3ba8743a70774d2ccb71
thebellywolf <cdspenzer@gmail.com>
2024-04-23 20:46:36 -0400

    added test sql for work experience achievement image

--- dotnet/test_database/capstone_test.sql
+++ dotnet/test_database/capstone_test.sql
@@ -1099,7 +1099,13 @@ RETURNING id INTO test_achievement_2_id;
 INSERT INTO work_experience_achievements (experience_id, achievement_id)
 VALUES (test_work_experience_1_id, test_achievement_2_id);
 
---Creating test portfolio background work experience 1 achievement images
+--Creating test portfolio background work experience 1 achievement 1 image
+INSERT INTO images (name, url)
+VALUES ('Achievement 1 Image', 'Achievement1Image.jpeg')
+RETURNING id INTO test_achievement_1_image_id;
+
+INSERT INTO achievement_images (achievement_id, image_id)
+VALUES (test_achievement_1_id, test_achievement_1_image_id);
 
 
 COMMIT TRANSACTION;
