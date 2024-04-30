352db1ba5dafc92a6fbd4467a8eab9d715215774
thebellywolf <cdspenzer@gmail.com>
2024-04-30 14:52:53 -0400

    initial setup file for PortfolioPostgresDaoTests

---
+++ dotnet/DevPortfolioTests/PortfolioPostgresDaoTests.cs
@@ -0,0 +1,16 @@
+using Capstone.DAO;
+using Capstone.DAO.Interfaces;
+using Capstone.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq; 
+using System;
+using System.Collections.Generic;
+
+namespace Capstone.UnitTests.DAO
+{
+    [TestClass]
+    public class PortfolioPostgresDaoTests : PostgresDaoTestBase
+    {
+
+    }
+}
