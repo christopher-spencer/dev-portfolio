9644d845f5c22a20d4af85e18bf1d62e3f941ca5
thebellywolf <cdspenzer@gmail.com>
2024-05-02 14:39:12 -0400

    organizing PostgresDaoTestBase

--- dotnet/DevPortfolioTests/PostgresDaoTestBase.cs
+++ dotnet/DevPortfolioTests/PostgresDaoTestBase.cs
@@ -26,25 +26,55 @@ namespace Capstone.UnitTests.DAO
 
                 // Assert.AreEqual(1, count, "Insert into user failed");
 
-                // Read user-related SQL queries from the test_users.sql file
-                string userSqlFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\test_database\test_users.sql");
-                string userSqlQueries = File.ReadAllText(userSqlFilePath);
+        /*  
+            **********************************************************************************************
+                                              Read & Execute test_schema.sql File
+            **********************************************************************************************
+        */
 
-                // Split user-related SQL queries by semicolon and execute them one by one
-                string[] userQueries = userSqlQueries.Split(';', StringSplitOptions.RemoveEmptyEntries);
-                foreach (string query in userQueries)
+                // Read SQL queries from the test_schema.sql file
+                string schemaSqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\test_database\test_schema.sql");
+                string schemaSqlQueries = File.ReadAllText(schemaSqlFile);
+
+                // Split SQL queries by semicolon and execute them one by one
+                string[] schemaQueries = schemaSqlQueries.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                foreach (string query in schemaQueries)
                 {
                     NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
                     cmd.ExecuteNonQuery();
                 }
 
+        /*  
+            **********************************************************************************************
+                                              Read & Execute test_data.sql File
+            **********************************************************************************************
+        */
+
                 // Read SQL queries from the test_data.sql file
                 string dataSqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\test_database\test_data.sql");
-                string sqlQueries = File.ReadAllText(dataSqlFile);
+                string dataSqlQueries = File.ReadAllText(dataSqlFile);
 
                 // Split SQL queries by semicolon and execute them one by one
-                string[] queries = sqlQueries.Split(';', StringSplitOptions.RemoveEmptyEntries);
-                foreach (string query in queries)
+                string[] dataQueries = dataSqlQueries.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                foreach (string query in dataQueries)
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+                }
+
+        /*  
+            **********************************************************************************************
+                                              Read & Execute test_users.sql File
+            **********************************************************************************************
+        */
+
+                // Read user-related SQL queries from the test_users.sql file
+                string userSqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\test_database\test_users.sql");
+                string userSqlQueries = File.ReadAllText(userSqlFile);
+
+                // Split user-related SQL queries by semicolon and execute them one by one
+                string[] userQueries = userSqlQueries.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                foreach (string query in userQueries)
                 {
                     NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
                     cmd.ExecuteNonQuery();
