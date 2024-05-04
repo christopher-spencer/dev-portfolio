50eb95be4701e43f263d00da5a07acdb391c426b
thebellywolf <cdspenzer@gmail.com>
2024-05-04 15:01:44 -0400

    test PGDaoTestBase updates

--- dotnet/DevPortfolioTests/PostgresDaoTestBase.cs
+++ dotnet/DevPortfolioTests/PostgresDaoTestBase.cs
@@ -13,27 +13,46 @@ namespace Capstone.UnitTests.DAO
         public void Initialize()
         {
             transaction = new TransactionScope();
-// FIXME check exceptions here and work on getting tests rolling
+            // FIXME check exceptions here and work on getting tests rolling
             using (NpgsqlConnection conn = new NpgsqlConnection(TestConnectionString))
             {
                 conn.Open();
 
                 // string sql_insert = "INSERT INTO users (username, password_hash, salt, user_role) " +
                 //     "VALUES ('notauser', 'jjjjjjjjj', 'kkkkkkkkkk', 'user');";
-                    
+
                 // NpgsqlCommand cmd = new NpgsqlCommand(sql_insert, conn);
                 // int count = cmd.ExecuteNonQuery();
 
                 // Assert.AreEqual(1, count, "Insert into user failed");
 
-        /*  
-            **********************************************************************************************
-                                              Read & Execute test_schema.sql File
-            **********************************************************************************************
-        */
+                /*  
+                    **********************************************************************************************
+                                                      Read & Execute test_users.sql File
+                    **********************************************************************************************
+                */
+
+                // Read user-related SQL queries from the test_users.sql file
+                string userSqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../test_database/test_users.sql");
+                string userSqlQueries = File.ReadAllText(userSqlFile);
+
+                // Split user-related SQL queries by semicolon and execute them one by one
+                string[] userQueries = userSqlQueries.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                foreach (string query in userQueries)
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+                }
+
+
+                /*  
+                    **********************************************************************************************
+                                                      Read & Execute test_schema.sql File
+                    **********************************************************************************************
+                */
 
                 // Read SQL queries from the test_schema.sql file
-                string schemaSqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\test_database\test_schema.sql");
+                string schemaSqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../test_database/test_schema.sql");
                 string schemaSqlQueries = File.ReadAllText(schemaSqlFile);
 
                 // Split SQL queries by semicolon and execute them one by one
@@ -44,14 +63,14 @@ namespace Capstone.UnitTests.DAO
                     cmd.ExecuteNonQuery();
                 }
 
-        /*  
-            **********************************************************************************************
-                                              Read & Execute test_data.sql File
-            **********************************************************************************************
-        */
+                /*  
+                    **********************************************************************************************
+                                                      Read & Execute test_data.sql File
+                    **********************************************************************************************
+                */
 
                 // Read SQL queries from the test_data.sql file
-                string dataSqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\test_database\test_data.sql");
+                string dataSqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../test_database/test_data.sql");
                 string dataSqlQueries = File.ReadAllText(dataSqlFile);
 
                 // Split SQL queries by semicolon and execute them one by one
@@ -62,23 +81,6 @@ namespace Capstone.UnitTests.DAO
                     cmd.ExecuteNonQuery();
                 }
 
-        /*  
-            **********************************************************************************************
-                                              Read & Execute test_users.sql File
-            **********************************************************************************************
-        */
-
-                // Read user-related SQL queries from the test_users.sql file
-                string userSqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\test_database\test_users.sql");
-                string userSqlQueries = File.ReadAllText(userSqlFile);
-
-                // Split user-related SQL queries by semicolon and execute them one by one
-                string[] userQueries = userSqlQueries.Split(';', StringSplitOptions.RemoveEmptyEntries);
-                foreach (string query in userQueries)
-                {
-                    NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
-                    cmd.ExecuteNonQuery();
-                }
             }
         }
 
