b2aebb7a6442ca72f78a8ae51efc0778c106280c
thebellywolf <cdspenzer@gmail.com>
2024-05-22 19:49:09 -0400

    deleted fixme

--- dotnet/DevPortfolioTests/PortfolioPostgresDaoTests.cs
+++ dotnet/DevPortfolioTests/PortfolioPostgresDaoTests.cs
@@ -12,5 +12,31 @@ namespace Capstone.UnitTests.DAO
     public class PortfolioPostgresDaoTests : PostgresDaoTestBase
     {
 
+        [TestMethod]
+
+        public void GetPortfolios_Returns_All_Portfolios()
+        {
+            // Arrange
+            var imageDaoMock = new Mock<IImageDao>();
+            PortfolioPostgresDao dao = new PortfolioPostgresDao(TestConnectionString, imageDaoMock.Object);
+
+            // Act
+            List<Portfolio> portfolios = dao.GetPortfolios();
+
+            // Assert
+            Assert.IsNotNull(portfolios);
+            Assert.IsTrue(portfolios.Count > 0);
+
+            // Additional assertions for properties
+            foreach (var portfolio in portfolios)
+            {
+                Assert.IsNotNull(portfolio.Id);
+                Assert.IsNotNull(portfolio.Name);
+                Assert.IsNotNull(portfolio.Location);
+                Assert.IsNotNull(portfolio.ProfessionalSummary);
+                Assert.IsNotNull(portfolio.Email);
+            }
+        }
+
     }
 }
