56d787c8c4e30e2bc04710298a4777e5c1c4b820
thebellywolf <cdspenzer@gmail.com>
2024-05-23 14:01:38 -0400

    GetPortfolioById_Returns_Correct_Portfolio

--- dotnet/DevPortfolioTests/PortfolioPostgresDaoTests.cs
+++ dotnet/DevPortfolioTests/PortfolioPostgresDaoTests.cs
@@ -51,5 +51,41 @@ namespace Capstone.UnitTests.DAO
             }
         }
 
+        [TestMethod]
+        public void GetPortfolioById_Returns_Correct_Portfolio()
+        {
+            // Arrange
+            var sideProjectDaoMock = new Mock<ISideProjectDao>();
+            var websiteDaoMock = new Mock<IWebsiteDao>();
+            var imageDaoMock = new Mock<IImageDao>();
+            var skillDaoMock = new Mock<ISkillDao>();
+            var workExperienceDaoMock = new Mock<IWorkExperienceDao>();
+            var educationDaoMock = new Mock<IEducationDao>();
+            var credentialDaoMock = new Mock<ICredentialDao>();
+            var volunteerWorkDaoMock = new Mock<IVolunteerWorkDao>();
+            var openSourceContributionDaoMock = new Mock<IOpenSourceContributionDao>();
+            var hobbyDaoMock = new Mock<IHobbyDao>();
+
+            PortfolioPostgresDao dao = new PortfolioPostgresDao(TestConnectionString, sideProjectDaoMock.Object, 
+            websiteDaoMock.Object, imageDaoMock.Object, skillDaoMock.Object, workExperienceDaoMock.Object, 
+            educationDaoMock.Object, credentialDaoMock.Object, volunteerWorkDaoMock.Object, 
+            openSourceContributionDaoMock.Object, hobbyDaoMock.Object);
+
+            // Act
+            Portfolio portfolio = dao.GetPortfolio(1);
+
+            // Assert
+            Assert.IsNotNull(portfolio);
+            Assert.AreEqual(1, portfolio.Id);
+
+            // Additional assertions for properties
+            Assert.IsNotNull(portfolio.Name);
+            Assert.IsNotNull(portfolio.Location);
+            Assert.IsNotNull(portfolio.ProfessionalSummary);
+            Assert.IsNotNull(portfolio.Email);
+        }
+
+        
+
     }
 }
