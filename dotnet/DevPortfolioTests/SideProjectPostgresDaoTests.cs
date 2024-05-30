dac1d33539db192f51c68a9e052a4beafea7ab50
thebellywolf <cdspenzer@gmail.com>
2024-05-30 15:53:18 -0400

    TestIntialize setup

--- dotnet/DevPortfolioTests/SideProjectPostgresDaoTests.cs
+++ dotnet/DevPortfolioTests/SideProjectPostgresDaoTests.cs
@@ -19,6 +19,46 @@ namespace Capstone.UnitTests.DAO
         private Mock<IApiServiceDao> apiServiceDaoMock = null!;
         private Mock<IWebsiteDao> websiteDaoMock = null!;
         private Mock<IDependencyLibraryDao> dependencyLibraryDaoMock = null!;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            try
+            {
+                base.Initialize();
+                InitializeMocks();
+                dao = CreateDaoWithMocks();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception during TestInitialize: {ex}");
+                throw;
+            }
+        }
+
+        private void InitializeMocks()
+        {
+            goalDaoMock = new Mock<IGoalDao>();
+            imageDaoMock = new Mock<IImageDao>();
+            skillDaoMock = new Mock<ISkillDao>();
+            contributorDaoMock = new Mock<IContributorDao>();
+            apiServiceDaoMock = new Mock<IApiServiceDao>();
+            websiteDaoMock = new Mock<IWebsiteDao>();
+            dependencyLibraryDaoMock = new Mock<IDependencyLibraryDao>();
+        }
+
+        private SideProjectPostgresDao CreateDaoWithMocks()
+        {
+            return new SideProjectPostgresDao(
+                TestConnectionString,
+                goalDaoMock.Object,
+                imageDaoMock.Object,
+                skillDaoMock.Object,
+                contributorDaoMock.Object,
+                apiServiceDaoMock.Object,
+                websiteDaoMock.Object,
+                dependencyLibraryDaoMock.Object);
+        }
     
     }
 }
