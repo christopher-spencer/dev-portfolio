ffad59f271fb0397509ad49a37f2e9aa26686975
thebellywolf <cdspenzer@gmail.com>
2024-06-08 15:13:21 -0400

    setup EducationPostgresDAOTests file

---
+++ dotnet/DevPortfolioTests/EducationPostgresDAOTests.cs
@@ -0,0 +1,15 @@
+using Capstone.DAO;
+using Capstone.DAO.Interfaces;
+using Capstone.Models;
+using Moq;
+
+namespace Capstone.UnitTests.DAO
+{
+    [TestClass]
+    public class EducationPostgresDAOTests : PostgresDaoTestBase
+    {
+
+        
+
+    }
+}
