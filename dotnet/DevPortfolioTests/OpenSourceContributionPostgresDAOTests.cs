5fe1aedeacbe4c2932c58766b98a7db9662314e0
thebellywolf <cdspenzer@gmail.com>
2024-06-12 16:26:46 -0400

    created opc test objects

--- dotnet/DevPortfolioTests/OpenSourceContributionPostgresDAOTests.cs
+++ dotnet/DevPortfolioTests/OpenSourceContributionPostgresDAOTests.cs
@@ -41,6 +41,30 @@ namespace Capstone.UnitTests.DAO
             );
         }   
 
-        
+        private OpenSourceContribution CreateAnOpenSourceContributionTestObject1()
+        {
+            return new OpenSourceContribution
+            {
+                ProjectName = "Test Project Name",
+                OrganizationName = "Test Organization Name",
+                StartDate = new DateTime(2021, 1, 1),
+                EndDate = new DateTime(2021, 1, 31),
+                ProjectDescription = "Test Project Description",
+                ContributionDetails = "Test Contribution Details",
+            };
+        }
+
+        private OpenSourceContribution CreateAnOpenSourceContributionTestObject2()
+        {
+            return new OpenSourceContribution
+            {
+                ProjectName = "Test Project Name 2",
+                OrganizationName = "Test Organization Name 2",
+                StartDate = new DateTime(2021, 2, 1),
+                EndDate = new DateTime(2021, 2, 28),
+                ProjectDescription = "Test Project Description 2",
+                ContributionDetails = "Test Contribution Details 2",
+            };
+        }
     }
 }
