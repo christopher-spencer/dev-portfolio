792b559956b1991a652b42b75875446247f1537a
thebellywolf <cdspenzer@gmail.com>
2024-06-18 10:48:30 -0400

    FIXME need to create test data for achievements tests to work properly reminder

--- dotnet/DevPortfolioTests/AchievementPostgresDaoTests.cs
+++ dotnet/DevPortfolioTests/AchievementPostgresDaoTests.cs
@@ -31,35 +31,67 @@ namespace Capstone.UnitTests.DAO
                 imageDaoMock.Object);
         }
 
-        private Achievement CreateAWorkExperienceAchievementTestObject()
+        private Achievement CreateAWorkExperienceAchievementTestObject1()
         {
             return new Achievement
             {
-                Description = "Test work experience achievement"
+                Description = "Test work experience achievement 1"
             };
         }
 
-        private Achievement CreateAnEducationAchievementTestObject()
+        private Achievement CreateAWorkExperienceAchievementTestObject2()
         {
             return new Achievement
             {
-                Description = "Test education achievement"
+                Description = "Test work experience achievement 2"
             };
         }
 
-        private Achievement CreateAnOpenSourceContributionAchievementTestObject()
+        private Achievement CreateAnEducationAchievementTestObject1()
         {
             return new Achievement
             {
-                Description = "Test open source contribution achievement"
+                Description = "Test education achievement 1"
             };
         }
 
-        private Achievement CreateAVolunteerWorkAchievementTestObject()
+        private Achievement CreateAnEducationAchievementTestObject2()
         {
             return new Achievement
             {
-                Description = "Test volunteer work achievement"
+                Description = "Test education achievement 2"
+            };
+        }
+
+        private Achievement CreateAnOpenSourceContributionAchievementTestObject1()
+        {
+            return new Achievement
+            {
+                Description = "Test open source contribution achievement 1"
+            };
+        }
+
+        private Achievement CreateAnOpenSourceContributionAchievementTestObject2()
+        {
+            return new Achievement
+            {
+                Description = "Test open source contribution achievement 2"
+            };
+        }
+
+        private Achievement CreateAVolunteerWorkAchievementTestObject1()
+        {
+            return new Achievement
+            {
+                Description = "Test volunteer work achievement 1"
+            };
+        }
+
+        private Achievement CreateAVolunteerWorkAchievementTestObject2()
+        {
+            return new Achievement
+            {
+                Description = "Test volunteer work achievement 2"
             };
         }
 
@@ -68,7 +100,6 @@ namespace Capstone.UnitTests.DAO
             imageDaoMock.Setup(m => m.GetImageByAchievementId(It.IsAny<int>())).Returns(new Image());
         }
 
-// TODO add Unit Tests for AchievementPostgresDAOTests
 // FIXME need to build WorkExperience, Education, OpenSourceContribution, and VolunteerWork test data (which all hold achievements)
         [TestMethod]
         public void GetAchievements_Returns_All_Achievements()
@@ -76,23 +107,49 @@ namespace Capstone.UnitTests.DAO
             // Arrange
             int testId = 1;
 
-            Achievement workExperienceAchievement = CreateAWorkExperienceAchievementTestObject();
-            Achievement educationAchievement = CreateAnEducationAchievementTestObject();
-            Achievement openSourceContributionAchievement = CreateAnOpenSourceContributionAchievementTestObject();
-            Achievement volunteerWorkAchievement = CreateAVolunteerWorkAchievementTestObject();
+            Achievement workExperienceAchievement1 = CreateAWorkExperienceAchievementTestObject1();
+            Achievement workExperienceAchievement2 = CreateAWorkExperienceAchievementTestObject2();
 
-            dao.CreateAchievementByWorkExperienceId(testId, workExperienceAchievement);
-            dao.CreateAchievementByEducationId(testId, educationAchievement);
-            dao.CreateAchievementByOpenSourceContributionId(testId, openSourceContributionAchievement);
-            dao.CreateAchievementByVolunteerWorkId(testId, volunteerWorkAchievement);
+            Achievement educationAchievement1 = CreateAnEducationAchievementTestObject1();
+            Achievement educationAchievement2 = CreateAnEducationAchievementTestObject2();
+
+            Achievement openSourceContributionAchievement1 = CreateAnOpenSourceContributionAchievementTestObject1();
+            Achievement openSourceContributionAchievement2 = CreateAnOpenSourceContributionAchievementTestObject2();
+            
+            Achievement volunteerWorkAchievement1 = CreateAVolunteerWorkAchievementTestObject1();
+            Achievement volunteerWorkAchievement2 = CreateAVolunteerWorkAchievementTestObject2();
+
+            dao.CreateAchievementByWorkExperienceId(testId, workExperienceAchievement1);
+            dao.CreateAchievementByWorkExperienceId(testId, workExperienceAchievement2);
+
+            dao.CreateAchievementByEducationId(testId, educationAchievement1);
+            dao.CreateAchievementByEducationId(testId, educationAchievement2);
+
+            dao.CreateAchievementByOpenSourceContributionId(testId, openSourceContributionAchievement1);
+            dao.CreateAchievementByOpenSourceContributionId(testId, openSourceContributionAchievement2);
+
+            dao.CreateAchievementByVolunteerWorkId(testId, volunteerWorkAchievement1);
+            dao.CreateAchievementByVolunteerWorkId(testId, volunteerWorkAchievement2);
 
             SetupAchievementNestedDaoMockObjects();
             
+            // Act
             List<Achievement> achievements = dao.GetAchievements();
 
             // Assert
             Assert.AreEqual(4, achievements.Count);
-            
+
+            Assert.AreEqual(workExperienceAchievement1.Description, achievements[0].Description);
+            Assert.AreEqual(workExperienceAchievement2.Description, achievements[1].Description);
+
+            Assert.AreEqual(educationAchievement1.Description, achievements[0].Description);
+            Assert.AreEqual(educationAchievement2.Description, achievements[1].Description);
+
+            Assert.AreEqual(openSourceContributionAchievement1.Description, achievements[0].Description);
+            Assert.AreEqual(openSourceContributionAchievement2.Description, achievements[1].Description);
+
+            Assert.AreEqual(volunteerWorkAchievement1.Description, achievements[0].Description);
+            Assert.AreEqual(volunteerWorkAchievement2.Description, achievements[1].Description);
         }
 
     }
